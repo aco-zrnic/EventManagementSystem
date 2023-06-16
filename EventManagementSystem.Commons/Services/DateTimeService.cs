0cbcefdf2fadb99f9e9a0470a8d2b32a987a746b
aco-zrnic <a_zrnic@yahoo.com>
2023-06-16 09:44:47 +0200

    add datetime service

---
+++ EventManagementSystem.Commons/Services/DateTimeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem.Commons.Services
+{
+    public class DateTimeService : IDateTimeService
+    {
+        public DateTimeOffset UtcTime
+        {
+            get => DateTimeOffset.UtcNow;
+        }
+    }
+}
