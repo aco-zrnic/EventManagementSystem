09dae4b13b2e169759395e4dd2d7d4b1b5da7fe7
aco-zrnic <a_zrnic@yahoo.com>
2023-06-25 16:51:13 +0200

    make entity participantid nullable

--- EventManagementSystem.Web/Entities/Ticket.cs
+++ EventManagementSystem.Web/Entities/Ticket.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public int EventId { get; set; }
+        public int? ParticipantId { get; set; }
         public decimal Price { get; set; }
         public string TicketType { get; set; }
 
--- EventManagementSystem.Web/EventManagementAutomapperProfile.cs
+++ EventManagementSystem.Web/EventManagementAutomapperProfile.cs
@@ -10,7 +10,11 @@ namespace EventManagementSystem.Web
         public EventManagementAutomapperProfile()
         {
             CreateMap<EventRequest, Event>();
+            CreateMap<Event, EventOverride>();
             CreateMap<Event, EventResponse>();
+
+            CreateMap<Ticket, TicketResponse>()
+                .ForMember(a => a.Event, opt => opt.MapFrom(a => a.Event));
         }
     }
 }
