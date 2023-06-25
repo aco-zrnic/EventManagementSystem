bbb53c2e8125e2f25edf7cec8d72766ba2f6f562
aco-zrnic <a_zrnic@yahoo.com>
2023-06-25 16:50:12 +0200

    add new migration,make participantid nullable for ticket enitity

--- EventManagementSystem.Web/Migrations/20230620191256_MakeParticipantIdNullable.Designer.cs
+++ EventManagementSystem.Web/Migrations/20230620191256_MakeParticipantIdNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EventManagementSystem.Web.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace EventManagementSystem.Web.Migrations
 {
     [DbContext(typeof(EmContext))]
-    partial class EmContextModelSnapshot : ModelSnapshot
+    [Migration("20230620191256_MakeParticipantIdNullable")]
+    partial class MakeParticipantIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EventManagementSystem.Web/Migrations/20230620191256_MakeParticipantIdNullable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventManagementSystem.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeParticipantIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "participant_id",
+                table: "tickets",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "participant_id",
+                table: "tickets",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true,
+                oldType: "integer"
+            );
+        }
+    }
+}
