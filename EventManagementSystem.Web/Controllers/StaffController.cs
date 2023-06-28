22f0191742bb9dfd3307587bb8b82e00ad3a61a9
aco-zrnic <a_zrnic@yahoo.com>
2023-06-28 15:28:54 +0200

    add new staffcontroller

---
+++ EventManagementSystem.Web/Controllers/StaffController.cs
@@ -0,0 +1,38 @@
+﻿using EventManagementSystem.Web.Dto.Request;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EventManagementSystem.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        // GET: api/<StaffController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StaffController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StaffController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] StuffRequest request) { }
+
+        // PUT api/<StaffController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) { }
+
+        // DELETE api/<StaffController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
+    }
+}
---
+++ EventManagementSystem.Web/Dto/Request/StuffRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace EventManagementSystem.Web.Dto.Request
+{
+    public class StuffRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public int EventId { get; set; }
+    }
+}
