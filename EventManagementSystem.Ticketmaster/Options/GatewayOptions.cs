86e3d6890ee6b27fc983a0ded14cda5faedf75bc
aco-zrnic <a_zrnic@yahoo.com>
2023-07-25 15:41:10 +0200

    add gateway options

---
+++ EventManagementSystem.Ticketmaster/Options/GatewayOptions.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManagementSystem.Ticketmaster.Options
+{
+    public class GatewayOptions
+    {
+        [Required]
+        [Url]
+        public string Url { get; set; }
+
+        [Required]
+        public string Version { get; set; }
+
+        [Required]
+        public string Package { get; set; }
+
+        public string ApiKey { get; set; }
+    }
+}
