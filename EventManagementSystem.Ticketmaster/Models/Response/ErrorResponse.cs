918da98492d395333121d55ce76a9d3abc875273
aco-zrnic <a_zrnic@yahoo.com>
2023-07-26 10:14:48 +0200

    add request/response models in ticketmaster service

---
+++ EventManagementSystem.Ticketmaster/Models/Response/ErrorResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace EventManagementSystem.Ticketmaster.Models.Response
+{
+    public class Error
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        [JsonProperty("code")]
+        public int Code { get; set; }
+        [JsonProperty("httpCode")]
+        public int HttpCode { get; set; }
+    }
+
+    public class ErrorResponse
+    {
+        [JsonProperty("error")]
+        public Error Error { get; set; }
+    }
+}
