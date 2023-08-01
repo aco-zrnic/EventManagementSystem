3c92fe9571aceb2d79189c6fd977eb4ceff52021
aco-zrnic <a_zrnic@yahoo.com>
2023-08-01 15:16:41 +0200

    add new conductor project

---
+++ EventManagementSystem.Conductor/Program.cs
@@ -0,0 +1,83 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using ConductorSharp.Engine;
+using ConductorSharp.Engine.Extensions;
+using EventManagementSystem.Commons.Services;
+using EventManagementSystem.Commons.Behavior;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Filters;
+using EventManagementSystem.Conductor.Definitions;
+
+
+var appBuilder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+appBuilder.Host
+    .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+    .ConfigureContainer<ContainerBuilder>(
+        builder =>
+        {
+            builder.RegisterModule<WorkflowModule>();
+
+            builder
+                .AddConductorSharp(
+                    apiPath: appBuilder.Configuration.GetValue<string>("Conductor:ApiUrl"),
+                    baseUrl: appBuilder.Configuration.GetValue<string>("Conductor:BaseUrl"),
+                    preventErrorOnBadRequest: appBuilder.Configuration.GetValue<bool>(
+                        "Conductor:PreventErrorOnBadRequest"
+                    )
+                )
+                .AddExecutionManager(
+                    maxConcurrentWorkers: appBuilder.Configuration.GetValue<int>(
+                        "Conductor:MaxConcurrentWorkers"
+                    ),
+                    sleepInterval: appBuilder.Configuration.GetValue<int>(
+                        "Conductor:SleepInterval"
+                    ),
+                    longPollInterval: appBuilder.Configuration.GetValue<int>(
+                        "Conductor:LongPollInterval"
+                    ),
+                    domain: appBuilder.Configuration.GetValue<string>("Conductor:WorkerDomain"),
+                    handlerAssemblies: typeof(Program).Assembly
+                )
+                .AddPipelines(
+                    pipelines =>
+                    {
+                        pipelines.AddContextLogging();
+                        pipelines.AddRequestResponseLogging();
+                        pipelines.AddValidation();
+                    }
+                );
+
+            builder.RegisterType<DateTimeService>().As<IDateTimeService>();
+            builder.RegisterType<LoggingActionFilter>();
+        }
+    );
+
+
+appBuilder.Services.AddControllers();
+appBuilder.Services.AddRouting(options => options.LowercaseUrls = true);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+appBuilder.Services.AddEndpointsApiExplorer();
+appBuilder.Services.AddSwaggerGen();
+
+
+var app = appBuilder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+
+app.Run();
