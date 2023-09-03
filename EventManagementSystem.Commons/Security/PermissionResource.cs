8903b52545c099e7c9bd31a30e65d5f20f3bba43
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-09-03 16:12:44 +0200

    add permission resource

---
+++ EventManagementSystem.Commons/Security/PermissionResource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem.Commons.Security
+{
+    public class PermissionResource
+    {
+        public const string Create = "CreateAccess";
+        public const string Update = "UpdateAccess";
+        public const string View = "ReadAccess";
+        public const string Delete = "DeleteAccess";
+    }
+}
