479c237da409d40a20bae7315620ad6635868c5a
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-09-25 15:17:25 +0200

    add pipeline behavior and register it

---
+++ EventManagementSystem.Commons/Behavior/LoggingPipelineBehavior.cs
@@ -0,0 +1,35 @@
+﻿using EventManagementSystem.Commons.Services;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem.Commons.Behavior
+{
+    public class LoggingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    {
+        private readonly ILogger<LoggingPipelineBehavior<TRequest, TResponse>> _logger;
+        private readonly IDateTimeService _dateTimeService;
+
+        public LoggingPipelineBehavior(ILogger<LoggingPipelineBehavior<TRequest, TResponse>> logger, IDateTimeService dateTimeService)
+        {
+            _logger = logger;
+            _dateTimeService = dateTimeService;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            _logger.LogInformation("Starting request {@RequestName}, {@DateTimeUtc}",typeof(TRequest).Name,_dateTimeService.UtcTime);
+
+            var result =  await next();
+
+            _logger.LogInformation("Completed request {@RequestName}, {@DateTimeUtc}", typeof(TRequest).Name, _dateTimeService.UtcTime);
+
+            return result;
+        }
+    }
+}
---
+++ EventManagementSystem.Web/Modules/PipelineBehaviorModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using ConductorSharp.Engine.Extensions;
+using EventManagementSystem.Commons.Behavior;
+using EventManagementSystem.Web.Handler;
+using MediatR;
+
+namespace EventManagementSystem.Web.Modules
+{
+    public class PipelineBehaviorModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterGeneric(typeof(LoggingPipelineBehavior<,>)).As(typeof(IPipelineBehavior<,>));
+        }
+    }
+}
