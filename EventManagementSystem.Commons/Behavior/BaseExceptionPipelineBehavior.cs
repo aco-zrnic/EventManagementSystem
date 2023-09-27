c29a30a08756b635363065be5983c78b313dcc31
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-09-27 15:22:17 +0200

    add BaseExceptionPipelineBehavior

---
+++ EventManagementSystem.Commons/Behavior/BaseExceptionPipelineBehavior.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem.Commons.Behavior
+{
+    public abstract class BaseExceptionPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            try
+            {
+                return await next();
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandling(ex);
+                throw;
+            }
+        }
+
+        protected abstract void ExceptionHandling(Exception exception);
+    }
+}
