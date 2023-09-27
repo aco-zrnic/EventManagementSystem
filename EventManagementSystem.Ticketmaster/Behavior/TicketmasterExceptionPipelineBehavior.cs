c7fca6b96c38b80dfe6e851366d321a6bb402972
aleksandar.zrnic <aleksandar.zrnic@codaxy.com>
2023-09-27 15:22:53 +0200

    add TicketmasterExceptionPipelineBehavior

---
+++ EventManagementSystem.Ticketmaster/Behavior/TicketmasterExceptionPipelineBehavior.cs
@@ -0,0 +1,17 @@
+﻿using EventManagementSystem.Commons.Behavior;
+using MediatR;
+
+namespace EventManagementSystem.Ticketmaster.Behavior
+{
+    public class TicketmasterExceptionPipelineBehavior<TRequest, TResponse> : BaseExceptionPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        protected override void ExceptionHandling(Exception exception)
+        {
+            Console.WriteLine(exception.ToString());
+            switch (exception)
+            {
+               
+            }
+        }
+    }
+}
